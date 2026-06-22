10fce902597af3033c7dd0a76fac07495ad4c894
Vaibhav Kaushal <iamvaibhav1077@gmail.com>
2026-06-22 14:00:00 +0530

    feat: add Razorpay payment gateway integration

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -2,13 +2,18 @@ const Razorpay = require("razorpay");
 
 let instance = null;
 
+/**
+ * Initialize Razorpay instance
+ * Payment features will be disabled if keys are not configured
+ */
 if (process.env.RAZORPAY_KEY && process.env.RAZORPAY_SECRET) {
   instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
     key_secret: process.env.RAZORPAY_SECRET,
   });
+  console.log("Razorpay payment gateway initialized");
 } else {
-  console.warn("Warning: Razorpay keys not set. Payment features disabled.");
+  console.warn("Warning: Razorpay keys not set. Payment features will be disabled.");
 }
 
 exports.instance = instance;
